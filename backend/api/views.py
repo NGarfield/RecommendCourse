9a7922f1177c6f1aee12773763acb52e8f983aab
NGarfield <nackzas458@gmail.com>
2021-03-12 19:07:10 +0700

    master

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,8 +1,10 @@
-from rest_framework import generics
+from rest_framework import generics,mixins
 from backend.models import Subject,Calender,Register,Group,SubjectCondition
 from backend.api.serializers import (SubjectSerializers,CalenderSerializer,
                                      RegisterSerializer,GroupSerializer,
                                      SubjectConditionSerializer)
+import json
+from django.http import HttpResponse
 
 class SubjectListCreateAPIView(generics.ListCreateAPIView):
     # queryset = Subject.objects.filter(name_group_id=2)
@@ -16,10 +18,21 @@ class SubjectDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = SubjectSerializers
 
 
-class CalenderListCreateAPIView(generics.ListCreateAPIView):
+class CalenderListCreateAPIView(mixins.ListModelMixin,
+                                mixins.CreateModelMixin,
+                                generics.GenericAPIView):
     # queryset = Subject.objects.filter(name_group_id=2)
     queryset = Calender.objects.all()
     serializer_class = CalenderSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self,request,*args,**kwargs):
+        mydata = json.loads(request.body)
+        print(mydata)
+        self.create(request, *args, **kwargs)
+        return HttpResponse("Got json data")
 
 class RegisterListCreateAPIView(generics.ListCreateAPIView):
     # queryset = Register.objects.filter(name_group_id=2)
